4e035d9816f16e0fa3312a07dfddf0b1d01cf602
AJ Harmond <ajharmond@gmail.com>
2019-04-02 16:15:22 -0600

    Seedfile and minor bugfix

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,65 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Course.destroy_all
+User.destroy_all
+
+1.times do
+  course = Course.create(
+    title: Faker::Educator.course_name
+  )
+  binding.pry
+  15.times do
+    section = Section.create(
+      title: Faker::ProgrammingLanguage.name,
+      course_id: course.id
+    )
+    binding.pry
+    5.times do
+      unit = Unit.create(
+        title: Faker::Hacker.noun,
+        section_id: section.id
+      )
+      5.times do
+        Content.create(
+          title: Faker::Hacker.verb,
+          content: Faker::Lorem.paragraph,
+          unit_id: unit.id
+        )
+      end
+    end
+  end
+end
+
+19.times do
+  user = User.create(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: Faker::Internet.email,
+    password: 'password',
+    admin: false
+  )
+  1.times do
+    Enrollment.create(
+      user_id: user.id,
+      course_id: Course.first.id,
+      role: 'student'
+    )
+  end
+end
+
+1.times do
+  user = User.create(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: 'test@test.com',
+    password: 'password',
+    admin: true
+  )
+  1.times do
+    Enrollment.create(
+      user_id: user.id,
+      course_id: Course.first.id,
+      role: 'staff'
+    )
+  end
+end
+
+puts 'Planted the seeds'
