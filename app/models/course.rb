fe77d9d9293ab518f4d882c5575e7e43876f88eb
AJ Harmond <ajharmond@gmail.com>
2019-04-02 15:12:31 -0600

    typo fix in course model

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,5 +1,5 @@
 class Course < ApplicationRecord
   has_many :sections, dependent: :destroy
   has_many :enrollments, dependent: :destroy
-  has_meny :users, through: :enrollments
+  has_many :users, through: :enrollments
 end
