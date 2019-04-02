13c0cb4d11462e79516b848eb3bfe19f3a985f16
3mpirical <3mpirical10510@gmail.com>
2019-04-02 13:03:11 -0600

    formatted with prettier

--- client/src/components/CourseWork.js
+++ client/src/components/CourseWork.js
@@ -2,35 +2,28 @@ import React from "react";
 import "../coursework.css";
 import CourseSection from "./CourseSection";
 
-
 class CourseWork extends React.Component {
-    state = { names: ["week1","week2","week3","week4","week5"] }
+  state = { names: ["week1", "week2", "week3", "week4", "week5"] };
 
-    renderSections = () => {
-        return this.state.names.map((name, index) => {
-            return <CourseSection key={index} name={name} />
-        })
-    }
+  renderSections = () => {
+    return this.state.names.map((name, index) => {
+      return <CourseSection key={index} name={name} />;
+    });
+  };
 
-    render() {
-        return (
-            <>
-                <div className="course-work__container" >
-                    <div className="section-container" >
-                        <h2 className="section-heading" >Course Work</h2>
+  render() {
+    return (
+      <>
+        <div className="course-work__container">
+          <div className="section-container">
+            <h2 className="section-heading">Course Work</h2>
 
-                        { this.renderSections() }
-                        {/* <div className="section" >week2 <div className="section-icon">+</div> </div>
-                        <div className="section" >week3 <div className="section-icon">+</div> </div>
-                        <div className="section" >week4 <div className="section-icon">+</div> </div>
-                        <div className="section" >week5 <div className="section-icon">+</div> </div> */}
-                    </div>
-                </div>
-            </>
-        )
-    }
+            {this.renderSections()}
+          </div>
+        </div>
+      </>
+    );
+  }
 }
-
-
 
 export default CourseWork;
--- client/src/components/Units.js
+++ client/src/components/Units.js
@@ -1,22 +1,15 @@
 import React from "react";
 
-
 class Units extends React.Component {
-    renderUnits = () => {
-        return this.props.units.map((unit) => {
-            return <div className="unit" >{unit.name}</div>
-        })
-    }
+  renderUnits = () => {
+    return this.props.units.map(unit => {
+      return <div className="unit">{unit.name}</div>;
+    });
+  };
 
-    render() {
-        return(
-            <div className="units-container">
-                {this.renderUnits()}
-            </div>
-        )
-    }
+  render() {
+    return <div className="units-container">{this.renderUnits()}</div>;
+  }
 }
-
-
 
 export default Units;
