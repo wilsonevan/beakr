58f9348f61ff14836ebddebfea8ac9e75224a5b0
AJ Harmond <ajharmond@gmail.com>
2019-04-02 13:51:46 -0600

    basic lecture view

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import Home from './components/Home';
 import NoMatch from './components/NoMatch';
 import Login from './components/Login';
 import Register from './components/Register';
+import LectureView from './components/LectureView';
 import FetchUser from './components/FetchUser';
 import { Switch, Route } from 'react-router-dom';
 import { Container, } from 'semantic-ui-react';
@@ -15,6 +16,7 @@ const App = () => (
           <Route exact path='/' component={Home} />
           <Route exact path='/login' component={Login} />
           <Route exact path='/register' component={Register} />
+          <Route exact path='/lecture' component={LectureView} />
           <Route component={NoMatch} />
         </Switch>
       </Container>
---
+++ client/src/components/LectureView.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import { Segment, Header, Icon, } from 'semantic-ui-react';
+
+class LectureView extends React.Component {
+  state = { title: '', content: '' }
+
+  componentDidMount() {
+    // fetch lecture info
+  }
+
+  render() {
+    const { title, content, } = this.state
+
+    return (
+      <>
+        <Header as={Link} to='/section/:section_id' content='< Course Work' color='green' size='huge' textAlign='left'/>
+        <BasicHeader style={{ color: '#23A24D' }}>
+          <Icon name='block layout' color='green' />
+            Lecture Title
+            {/* {title} */}
+        </BasicHeader>
+        <SegmentContent>
+          <BasicHeader>First Subsection</BasicHeader>
+          First Subsection content.
+          <BasicHeader>Second Subsection</BasicHeader>
+          Second Subsection content.
+          {/* {content} */}
+        </SegmentContent>
+      </>
+    )
+  }
+}
+
+const BasicHeader = styled.h3`
+  font-family: 'Courier New', Courier, serif
+`
+
+const SegmentContent = styled(Segment)`
+  font-family: 'Courier New', Courier, serif
+`
+
+export default LectureView
