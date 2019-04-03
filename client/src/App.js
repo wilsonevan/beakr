9e9fdcf87ca3a12e3fb60e02d6ac620456396bb1
AJ Harmond <ajharmond@gmail.com>
2019-04-03 15:00:56 -0600

    fix

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import NoMatch from './components/NoMatch';
 import Login from './components/Login';
 import Register from './components/Register';
 import Dashboard from './components/dashboard/Dashboard'
-import LectureView from './components/LectureView';
+import ContentView from './components/ContentView';
 import FetchUser from './components/FetchUser';
 import { Switch, Route } from 'react-router-dom';
 import { Container, } from 'semantic-ui-react';
@@ -14,20 +14,6 @@ import CourseWork from "./components/CourseWork";
 const App = () => (
   <>
     <FetchUser>
-<<<<<<< Updated upstream
-      <Navbar />
-      <Container>
-        <Switch>
-          <Route exact path='/' component={Home} />
-          <Route exact path='/login' component={Login} />
-          <Route exact path='/register' component={Register} />
-          <Route exact path='/dashboard' component={Dashboard} />
-          <Route exact path='/lecture' component={LectureView} />
-          <Route exact path='/coursework' component={CourseWork} />
-          <Route component={NoMatch} />
-        </Switch>
-      </Container>
-=======
       <Navbar>
         <Container as={AppContainer}>
           <Switch>
@@ -41,7 +27,6 @@ const App = () => (
           </Switch>
         </Container>
       </Navbar>
->>>>>>> Stashed changes
     </FetchUser>
   </>
 )
