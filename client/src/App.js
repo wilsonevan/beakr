7668d8a3bbfba6a3cdfd9bcc228d5cd53d4a4898
AJ Harmond <ajharmond@gmail.com>
2019-04-03 14:17:38 -0600

    rename and finish ContentView

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import NoMatch from './components/NoMatch';
 import Login from './components/Login';
 import Register from './components/Register';
 import Dashboard from './components/dashboard/Dashboard'
-import LectureView from './components/LectureView';
+import ContentView from './components/ContentView';
 import FetchUser from './components/FetchUser';
 import { Switch, Route } from 'react-router-dom';
 import { Container, } from 'semantic-ui-react';
@@ -15,34 +15,18 @@ import styled from 'styled-components';
 const App = () => (
   <>
     <FetchUser>
-<<<<<<< HEAD
       <Navbar />
       <Container>
         <Switch>
           <Route exact path='/' component={Home} />
           <Route exact path='/login' component={Login} />
           <Route exact path='/register' component={Register} />
-          <Route exact path='/units/:unit_id/contents/:id' component={LectureView} />
+          <Route exact path='/units/:unit_id/contents/:id' component={ContentView} />
           <Route exact path='/dashboard' component={Dashboard} />
           <Route exact path='/coursework' component={CourseWork} />
           <Route component={NoMatch} />
         </Switch>
       </Container>
-=======
-      <Navbar>     
-        <Container as={AppContainer}>
-          <Switch>
-            <Route exact path='/' component={Home} />
-            <Route exact path='/login' component={Login} />
-            <Route exact path='/register' component={Register} />
-            <Route exact path='/dashboard' component={Dashboard} />
-            <Route exact path='/lecture' component={LectureView} />
-            <Route exact path='/coursework' component={CourseWork} />
-            <Route component={NoMatch} />
-          </Switch>
-        </Container>
-      </Navbar>
->>>>>>> 537127626139b74367761d294007c9a3116731bf
     </FetchUser>
   </>
 )
--- client/src/components/ContentView.js
+++ client/src/components/ContentView.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
-import styled from 'styled-components';
 import { Segment, Header, Icon, } from 'semantic-ui-react';
 
-class LectureView extends React.Component {
+class ContentView extends React.Component {
   state = { title: '', content: '' }
 
   componentDidMount() {
@@ -21,24 +20,16 @@ class LectureView extends React.Component {
     return (
       <>
         <Header as={Link} to='/section/:section_id' content='< Course Work' color='green' size='huge' textAlign='left'/>
-        <BasicHeader style={{ color: '#23A24D' }}>
+        <Header style={{ color: '#23A24D' }}>
           <Icon name='block layout' color='green' />
             {title}
-        </BasicHeader>
-        <SegmentContent>
+        </Header>
+        <Segment>
           {content}
-        </SegmentContent>
+        </Segment>
       </>
     )
   }
 }
 
-const BasicHeader = styled.h3`
-  font-family: 'Courier New', Courier, serif
-`
-
-const SegmentContent = styled(Segment)`
-  font-family: 'Courier New', Courier, serif
-`
-
-export default LectureView
+export default ContentView
