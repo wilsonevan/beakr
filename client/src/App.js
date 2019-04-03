2937fbbf91f04757701651d35c59aa9fbc5eac0b
3mpirical <3mpirical10510@gmail.com>
2019-04-02 20:45:33 -0600

    protected route created, and added to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,40 +1,41 @@
-import React from 'react';
-import Home from './components/Home';
-import NoMatch from './components/NoMatch';
-import Login from './components/Login';
-import Register from './components/Register';
-import Dashboard from './components/dashboard/Dashboard'
-import LectureView from './components/LectureView';
-import FetchUser from './components/FetchUser';
-import { Switch, Route } from 'react-router-dom';
-import { Container, } from 'semantic-ui-react';
-import Navbar from './components/Navbar';
+import React from "react";
+import Home from "./components/Home";
+import NoMatch from "./components/NoMatch";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Dashboard from "./components/dashboard/Dashboard";
+import LectureView from "./components/LectureView";
+import FetchUser from "./components/FetchUser";
+import { Switch, Route } from "react-router-dom";
+import { Container } from "semantic-ui-react";
+import ProtectedRoute from "./components/ProtectedRoute";
+import Navbar from "./components/Navbar";
 import CourseWork from "./components/CourseWork";
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const App = () => (
   <>
     <FetchUser>
-      <Navbar>     
+      <Navbar>
         <Container as={AppContainer}>
           <Switch>
-            <Route exact path='/' component={Home} />
-            <Route exact path='/login' component={Login} />
-            <Route exact path='/register' component={Register} />
-            <Route exact path='/dashboard' component={Dashboard} />
-            <Route exact path='/lecture' component={LectureView} />
-            <Route exact path='/coursework' component={CourseWork} />
+            <Route exact path="/" component={Home} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/register" component={Register} />
+            <ProtectedRoute exact path="/dashboard" component={Dashboard} />
+            <ProtectedRoute exact path="/lecture" component={LectureView} />
+            <ProtectedRoute exact path="/coursework" component={CourseWork} />
             <Route component={NoMatch} />
           </Switch>
         </Container>
       </Navbar>
     </FetchUser>
   </>
-)
+);
 
 // This styling allows any App view and the Sidebar to fill up the entire page height
 const AppContainer = styled.div`
   height: 100vh;
-`
+`;
 
 export default App;
