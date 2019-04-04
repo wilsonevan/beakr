bb1acf3f5563a51409538cd89502f8487495a740
AJ Harmond <ajharmond@gmail.com>
2019-04-04 14:00:13 -0600

    whoops full merge this time

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
@@ -8,16 +7,6 @@ import * as serviceWorker from './serviceWorker';
 import 'semantic-ui-css/semantic.min.css';
 import 'react-quill/dist/quill.snow.css';
 import { initMiddleware } from 'devise-axios';
-=======
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import { BrowserRouter } from "react-router-dom";
-import { AuthProvider } from "./providers/AuthProvider";
-import * as serviceWorker from "./serviceWorker";
-import "semantic-ui-css/semantic.min.css";
-import { initMiddleware } from "devise-axios";
->>>>>>> a36ec54486b8f0a5169b87bb2eed095f9b1387c1
 
 initMiddleware();
 
