8f7e2bdb4fc0d8e27c1971d1af6895d495afc9b6
3mpirical <3mpirical10510@gmail.com>
2019-04-08 09:40:31 -0600

    added unit_content routes and content search routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,8 @@ Rails.application.routes.draw do
     resources :enrollments, only: [:create, :update, :destroy]
 
     resources :users, only: [:index]
-    resources :user_contents, only: [:index, :create, :destroy]
+    resources :unit_contents, only: [:index, :create, :destroy]
+    delete '/unit/:unit_id/contents/:content_id/unit_content', to: '/api/unit_contents#delete_by_unit_and_content'
     
     resources :courses do
       resources :sections
@@ -20,6 +21,7 @@ Rails.application.routes.draw do
       resources :contents
     end
 
-    get 'user_courses', to: 'courses#user_courses'
+    post 'contents/search', to: '/api/contents#search_contents'
+    post 'contents/search/:unit_id', to: '/api/contents#search_contents_not_in_unit'
   end
 end
