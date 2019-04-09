f7953947972f730870e8805e609dfb0d04bef8e2
Jaxon Porter <jaxonutah@gmail.com>
2019-04-09 10:23:50 -0600

    added enrollment_id to sql call for attendances

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -3,7 +3,7 @@ class Attendance < ApplicationRecord
 
   def self.get_attendances(course_id)
     User.find_by_sql([
-    "select record_date, u.id, attendance_record, u.first_name, u.last_name, course_id, attendance_id
+    "select record_date, u.id, attendance_record, u.first_name, u.last_name, course_id, attendance_id, new.enrollment_id
     from (select *, a.id as attendance_id from attendances as a left join enrollments as e on a.enrollment_id = e.id) as new
     full join users as u
     on new.user_id = u.id
