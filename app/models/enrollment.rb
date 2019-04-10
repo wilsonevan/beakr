58538a4a8f29b506186adb86d74ec6eed1913b9d
3mpirical <3mpirical10510@gmail.com>
2019-04-10 01:30:33 -0600

    added sql queries to find enrollments and users

--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -1,4 +1,12 @@
 class Enrollment < ApplicationRecord
   belongs_to :user
   belongs_to :course
+
+  def self.find_by_user_and_course(user_id, course_id)
+    Enrollment.find_by_sql(["
+      SELECT * FROM enrollments
+      WHERE user_id = ?
+      AND course_id = ?
+    ",user_id, course_id ]).first()
+  end
 end
