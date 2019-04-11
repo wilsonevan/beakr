a5d8f2ba97aab7672258dec358d17c48c7d92819
3mpirical <3mpirical10510@gmail.com>
2019-04-11 01:11:09 -0600

    added 'type' to assignments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_10_221600) do
+ActiveRecord::Schema.define(version: 2019_04_11_070858) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2019_04_10_221600) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "body"
+    t.string "type"
   end
 
   create_table "attendances", force: :cascade do |t|
