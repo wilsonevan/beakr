9818ad819360a07e00533caec57bfad3e494d493
AJ Harmond <ajharmond@gmail.com>
2019-04-12 14:01:40 -0600

    assignmentsubmission model, controller, routes

--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -2,6 +2,8 @@ class Enrollment < ApplicationRecord
   belongs_to :user
   belongs_to :course
   has_many :attendances, dependent: :destroy
+  has_many :assignment_submissions, dependent: :destroy
+  has_many :assignments, through: :assignment_submissions
 
   def self.find_by_user_and_course(user_id, course_id)
     Enrollment.find_by_sql(["
---
+++ test/controllers/api/assignmentsubmissions_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class Api::AssignmentsubmissionsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
