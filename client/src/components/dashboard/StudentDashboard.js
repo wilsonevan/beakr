edd0bd0bdcc29af3bce0e5c8445338bc393c1779
3mpirical <3mpirical10510@gmail.com>
2019-04-12 16:55:41 -0600

    removed unnecessary code

--- client/src/components/dashboard/StudentDashboard.js
+++ client/src/components/dashboard/StudentDashboard.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Menu, Segment, Header } from 'semantic-ui-react'
+import { Segment, Header } from 'semantic-ui-react'
 import axios from 'axios'
 import { Link, } from 'react-router-dom'
 import styled from 'styled-components'
