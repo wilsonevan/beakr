1f49e956e6d022903cb2071d07642a7c86da3a75
3mpirical <3mpirical10510@gmail.com>
2019-04-16 14:02:15 -0600

    updated prompt styling

--- client/src/components/quiz-view/QuizPrompt.js
+++ client/src/components/quiz-view/QuizPrompt.js
@@ -23,7 +23,7 @@ const PromptContainer = styled.div`
     transform: translate(-50%, -50%);
     left: 50%;
     top: 50%;
-    background-color: white;
+    background-color: #23a24d;
     border-radius: 10px;
     height: 20rem;
     width: 40rem;
@@ -34,17 +34,20 @@ const WhiteBackground = styled.div`
     transform: translate(-50%, -50%);
     left: 50%;
     top: 50%;
-    height: calc(100% - 0.5rem);
-    width: calc(100% - 0.5rem);
-    background-color: #23a24d;
+    // height: calc(100% - 0.5rem);
+    // width: calc(100% - 0.5rem);
+    height: 100%;
+    width: 100%;
+    background-color: #f7f7f7;
     border-radius: 10px;
     padding: 1rem;
+    box-shadow: 0 1px 1px 2px rgba(150,150,150,0.2);
 `
 
 const PromptText = styled.h2`
     position: relative;
     top: 1.5rem;
-    color: white;
+    color: #23a24d;
     text-align: center;
 `
 
@@ -56,25 +59,25 @@ const ButtonLeft = styled.button`
     text-decoration: none;
     border: none;
     border-radius: 5px;
-    padding: 0.5rem;
-    background-color: white;
-    color: #23a24d;
+    padding: 1rem;
+    background-color: #23a24d;
+    color: white;
     letter-spacing: 1.5px;
     cursor: pointer;
     font-family: "Poppins", sans-serif;
-    font-size: 1.75rem;
+    font-size: 1.5rem;
     outline: none;
     transition-duration: 0.2s;
 
     :hover {
-    color: white;
-    background-color: #2979ff;
+        color: white;
+        background-color: #41c36c;
     }
 
     :active {
-    box-shadow: 0 0 0 3px #41c36c;
-    background-color: white;
-    color: #41c36c;
+        box-shadow: 0 0 0 3px #41c36c;
+        background-color: white;
+        color: #41c36c;
     }
 `
 
@@ -86,25 +89,25 @@ const ButtonRight = styled.button`
     text-decoration: none;
     border: none;
     border-radius: 5px;
-    padding: 0.5rem;
-    background-color: white;
-    color: #23a24d;
+    padding: 1rem;
+    background-color: #23a24d;
+    color: white;
     letter-spacing: 1.5px;
     cursor: pointer;
     font-family: "Poppins", sans-serif;
-    font-size: 1.75rem;
+    font-size: 1.5rem;
     outline: none;
     transition-duration: 0.2s;
 
     :hover {
-    color: white;
-    background-color: #2979ff;
+        color: white;
+        background-color: #41c36c;
     }
 
     :active {
-    box-shadow: 0 0 0 3px #41c36c;
-    background-color: white;
-    color: #41c36c;
+        box-shadow: 0 0 0 3px #41c36c;
+        background-color: white;
+        color: #41c36c;
     }
 `
 
@@ -116,25 +119,25 @@ const ButtonCenter = styled.button`
     text-decoration: none;
     border: none;
     border-radius: 5px;
-    padding: 0.5rem;
-    background-color: white;
-    color: #23a24d;
+    padding: 1rem;
+    background-color: #23a24d;
+    color: white;
     letter-spacing: 1.5px;
     cursor: pointer;
     font-family: "Poppins", sans-serif;
-    font-size: 1.75rem;
+    font-size: 1.5rem;
     outline: none;
     transition-duration: 0.2s;
 
     :hover {
-    color: white;
-    background-color: #2979ff;
+        color: white;
+        background-color: #41c36c;
     }
 
     :active {
-    box-shadow: 0 0 0 3px #41c36c;
-    background-color: white;
-    color: #41c36c;
+        box-shadow: 0 0 0 3px #41c36c;
+        background-color: white;
+        color: #41c36c;
     }
 `
 
