b93075c117b82997b31b475efa5cd5c0313a93b5
AJ Harmond <ajharmond@gmail.com>
2019-04-22 16:41:38 -0600

    admin add user now works as intended and changed styling in assignmentview for admin

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -12,6 +12,15 @@ class Api::UsersController < ApplicationController
     render json: @user
   end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render json: @user
+    else
+      render json: @user.errors, status: 422
+    end
+  end
+
   def search_users
     render( json: User.search_users(params[:input]))
   end
@@ -93,6 +102,10 @@ class Api::UsersController < ApplicationController
 
   def set_user
     @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :admin)
   end
   
 end
